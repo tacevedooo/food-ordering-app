104c17be6bd7dd4ff5bb16d596487e6c5a45543d
tacevedooo <tacevedo@unal.edu.co>
2026-02-24 15:21:08 -0500

    backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,42 @@
-import express from 'express';
-import type { Request, Response } from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "dotenv/config";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
-    console.log('Connected to DataBase');
-}).catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const app = express(); 
-app.use(express.json());
+const app = express();
+
 app.use(cors());
 
-app.get('/test', async (req: Request, res: Response) => {
-    res.json({ message: 'Hello Test!' });
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
 });
 
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
+
 app.listen(7000, () => {
-    console.log('Server is running on localhost:7000');
+  console.log("server started on localhost:7000");
 });
